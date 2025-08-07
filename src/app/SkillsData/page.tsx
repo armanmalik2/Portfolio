a1adabe59e196afbb3f84eda0bfa61987d197d2e
armanmalik2 <armanmalik3370@gmail.com>
2025-08-07 12:35:07 +0530

    Update page.tsx

--- src/app/SkillsData/page.tsx
+++ src/app/SkillsData/page.tsx
@@ -3,18 +3,18 @@ type SkillsDataProps = {
     imgData : string
 }
 
-//export const SkillsData = ({imgLink , imgData}:SkillsDataProps) => {
-//    return(
-//        <div>
-//            
-//            <div className="m-2 w-26 border-1 flex flex-col justify-between items-center rounded-xl text-center hover:scale-110">
-//                    <img className="pt-2.5 pr-3 pb-0 pl-3 h-16" src={imgLink} />
-//                    <p className="m-2">{imgData}</p>
-//            </div>
-//                
- //       </div>
- //   );
-//};
+export const SkillsData = ({imgLink , imgData}:SkillsDataProps) => {
+    return(
+        <div>
+            
+            <div className="m-2 w-26 border-1 flex flex-col justify-between items-center rounded-xl text-center hover:scale-110">
+                    <img className="pt-2.5 pr-3 pb-0 pl-3 h-16" src={imgLink} />
+                    <p className="m-2">{imgData}</p>
+            </div>
+                
+        </div>
+    );
+};
 
 
 
@@ -27,5 +27,6 @@ const SkillsDataReturn = () => {
 }
 
 export default SkillsDataReturn;
+
 
 
